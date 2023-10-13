4ebde9c2d5239d53c8b06f6d75dcc9c0a5c258ef
UDHAYA KUMAR <udhaya642003@gmail.com>
2023-10-13 19:56:13 +0530

    project description

--- Components/Projects/Pro.js
+++ Components/Projects/Pro.js
@@ -25,9 +25,9 @@ const Project = () => {
           />
           <p className={classes.title}>Python</p>
           <p className={classes.desc}>
-            Python is a high-level, general-purpose programming language. Its
-            design philosophy emphasizes code readability with the use of
-            significant indentation
+            Voice based college students details management system using speech
+            recognization software in python. It simulates and process human
+            conversation, allowing human to interact with digital devices.
           </p>
         </div>
         <div
@@ -42,11 +42,11 @@ const Project = () => {
             src="https://www.bleepstatic.com/content/hl-images/2023/08/21/China-hacker.jpg"
             className={classes.img}
           />
-          <p className={classes.title}>Java script</p>
+          <p className={classes.title}>Python</p>
           <p className={classes.desc}>
-            JavaScript often abbreviated as JS, is a programming language that
-            is one of the coreWide Web, alongside of websites use JavaScript on
-            the client side
+            Distance learning in desired language. A e-learning platform enable
+            tutors to pursue courses in their comfortable language. Tutors can
+            also able to customize their study time as well.
           </p>
         </div>
         <div
@@ -61,11 +61,11 @@ const Project = () => {
             src="https://innovationatwork.ieee.org/wp-content/uploads/2020/03/bigstock-Isometric-Internet-Hacker-Atta-346273381_1024X684.png"
             className={classes.img}
           />
-          <p className={classes.title}>Java</p>
+          <p className={classes.title}>Python</p>
           <p className={classes.desc}>
-            Java is a high-level, class-based, object-oriented programming
-            language that is designed to have as few implementation dependencies
-            as possible
+            Fantasy team prediction using pandas. Pandas is a python library u
+            for analysing, cleaning and exploring of data. Predicting Fantasy
+            team using recently played matches.
           </p>
         </div>
       </div>
