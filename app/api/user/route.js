af31c42ec15b965650975c9a1a27fb6739c92d0c
udhay codes <udhaya642003@gmail.com>
2024-04-01 12:24:22 +0530

    check

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -4,30 +4,30 @@ import { NextResponse } from "next/server";
 const cl = await ConnectToDatabase();
 const client = await cl.connect();
 const db = await client.db("Portfolio");
-const collection = await db.collection("feedback");
-const date = new Date();
-const dat = new Date(date);
-const year = dat.getFullYear();
-const month = String(dat.getMonth() + 1).padStart(2, "0");
-const day = String(dat.getDate()).padStart(2, "0");
-const formattedDate = `${day}-${month}-${year}`;
-export async function POST(req) {
-  const data = await req.json();
-  const { Name, Email, Message } = data;
-  console.log({ Name, Email, Message });
-  const result = await collection.insertOne({
-    Name,
-    Email,
-    Message,
-    Date: formattedDate,
-  });
-  console.log(result);
-  if (result.acknowledged) {
-    return NextResponse.json({ msg: "Message sent successfully" });
-  } else {
-    return NextResponse.json({ msg: "Try again later" });
-  }
-}
+// const collection = await db.collection("feedback");
+// const date = new Date();
+// const dat = new Date(date);
+// const year = dat.getFullYear();
+// const month = String(dat.getMonth() + 1).padStart(2, "0");
+// const day = String(dat.getDate()).padStart(2, "0");
+// const formattedDate = `${day}-${month}-${year}`;
+// export async function POST(req) {
+//   const data = await req.json();
+//   const { Name, Email, Message } = data;
+//   console.log({ Name, Email, Message });
+//   const result = await collection.insertOne({
+//     Name,
+//     Email,
+//     Message,
+//     Date: formattedDate,
+//   });
+//   console.log(result);
+//   if (result.acknowledged) {
+//     return NextResponse.json({ msg: "Message sent successfully" });
+//   } else {
+//     return NextResponse.json({ msg: "Try again later" });
+//   }
+// }
 
 export async function GET() {
   const data = await collection.find({}).toArray();
