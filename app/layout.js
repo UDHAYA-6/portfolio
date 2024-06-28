11ce05a6ebe7bdb00def3845a08d56759b6387e2
udhay codes <udhaya642003@gmail.com>
2024-06-28 14:24:09 +0530

    google console

--- app/layout.js
+++ app/layout.js
@@ -50,20 +50,17 @@ import Head from "next/head";
 import "./globals.css";
 import HeaderMegaMenu from "@/components/Nav comoponent/navbar";
 
-// export const metadata = {
-//   title: "Udhaya Kumar, a next.js and react.js developer",
-//   description:
-//     "Explore Udhaya Kumar's portfolio featuring diverse web development projects. Skilled in React Js and Next Js, creates interactive, user-friendly websites.",
-//   keywords: "web development, portfolio, Udhaya, React js, Next.js",
-//   author: "Udhaya Kumar",
-//   image: "/profile.jpg",
-//   url: "https://portfolio-udhay.vercel.app",
-//   canonical: "https://portfolio-udhay.vercel.app",
-//   type: "website",
-//   alternates: {
-//     "google-site-verification": "alternates",
-//   },
-// };
+export const metadata = {
+  title: "Udhaya Kumar, a next.js and react.js developer",
+  description:
+    "Explore Udhaya Kumar's portfolio featuring diverse web development projects. Skilled in React Js and Next Js, creates interactive, user-friendly websites.",
+  keywords: "web development, portfolio, Udhaya, React js, Next.js",
+  author: "Udhaya Kumar",
+  image: "/profile.jpg",
+  url: "https://portfolio-udhay.vercel.app",
+  canonical: "https://portfolio-udhay.vercel.app",
+  type: "website",
+};
 
 export default function RootLayout({ children }) {
   const theme = createTheme({
