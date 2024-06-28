d3d03d31397ca4f2e444941ae980d3142de524fd
udhay codes <udhaya642003@gmail.com>
2024-06-28 17:27:04 +0530

    google console

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://your-site-url.com", // Replace with your site's URL
+  generateRobotsTxt: true, // (Optional) Generate robots.txt file
+  // Additional options if needed
+};
