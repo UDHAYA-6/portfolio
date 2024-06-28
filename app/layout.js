10b27f259e9fe5384a6eadc652560ee854f75f88
udhay codes <udhaya642003@gmail.com>
2024-06-28 14:20:59 +0530

    google console

--- app/layout.js
+++ app/layout.js
@@ -50,17 +50,20 @@ import Head from "next/head";
 import "./globals.css";
 import HeaderMegaMenu from "@/components/Nav comoponent/navbar";
 
-export const metadata = {
-  title: "Udhaya Kumar, a next.js and react.js developer",
-  description:
-    "Explore Udhaya Kumar's portfolio featuring diverse web development projects. Skilled in React Js and Next Js, creates interactive, user-friendly websites.",
-  keywords: "web development, portfolio, Udhaya, React js, Next.js",
-  author: "Udhaya Kumar",
-  image: "/profile.jpg",
-  url: "https://portfolio-udhay.vercel.app",
-  canonical: "https://portfolio-udhay.vercel.app",
-  type: "website",
-};
+// export const metadata = {
+//   title: "Udhaya Kumar, a next.js and react.js developer",
+//   description:
+//     "Explore Udhaya Kumar's portfolio featuring diverse web development projects. Skilled in React Js and Next Js, creates interactive, user-friendly websites.",
+//   keywords: "web development, portfolio, Udhaya, React js, Next.js",
+//   author: "Udhaya Kumar",
+//   image: "/profile.jpg",
+//   url: "https://portfolio-udhay.vercel.app",
+//   canonical: "https://portfolio-udhay.vercel.app",
+//   type: "website",
+//   alternates: {
+//     "google-site-verification": "alternates",
+//   },
+// };
 
 export default function RootLayout({ children }) {
   const theme = createTheme({
@@ -71,6 +74,14 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Head>
         <meta charSet="UTF-8" />
+        <meta
+          name="title"
+          content="Udhaya Kumar, a next.js and react.js developer"
+        />
+        <meta
+          name="desciption"
+          content="Explore Udhaya Kumar's portfolio featuring diverse web development projects. Skilled in React Js and Next Js, creates interactive, user-friendly websites."
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="google-site-verification"
